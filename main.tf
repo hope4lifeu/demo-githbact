670c37e8d2362245f62a3971522e22cddea581c1
ANDREWR <asazobor.richards@gmail.com>
2023-03-25 00:45:48 +0000

    updated

--- main.tf
+++ main.tf
@@ -1,13 +1,13 @@
-provider "aws" "vpc" {
+provider "aws" {
     region = "us-west-2"
   
 }
 
-resource "aws_vpc" "actions" {
+resource "aws_vpc" "action" {
     cidr_block = "10.0.0.0/16"
 
     tags = {
-      Name = "30githubdemo"
+      Name = "value"
     }
   
 }
